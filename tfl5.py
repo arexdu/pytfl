aa27f63591d85cf473324759a76ba8203eb8d1f5
arexdu <710776009@qq.com>
2017-09-05 13:38:54 +0800

    add tf neural network example

--- tfl5.py
+++ tfl5.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 ##add layer
 def add_layer(inputs, in_size, out_size, activation_function=None):
@@ -10,3 +11,28 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     else:
         outputs = activation_function(Wx_plus_b)
     return outputs
+
+##neural network
+x_data = np.linspace(-1,1,300)[:,np.newaxis]
+noise = np.random.normal(0,0.05,x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+xs = tf.placeholder(tf.float32,[None,1])
+ys = tf.placeholder(tf.float32,[None,1])
+
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                     reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(100000):
+        sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+        if i%10 == 0:
+            print(sess.run(loss,feed_dict={xs:x_data, ys: y_data}))
