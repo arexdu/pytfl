6683656f978a51ec484def4e04f2a03b14e02b3f
arexdu <710776009@qq.com>
2017-09-05 12:08:26 +0800

    add tf feed example

---
+++ tfl4.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+## feed, multiply
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+
+output = tf.multiply(input1, input2)
+
+with tf.Session() as sess:
+    print(sess.run(output, feed_dict={input1:[33.3], input2: [4.]}))
