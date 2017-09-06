815f31fdbfbf1d1e6b0d6527aee5e7382fdff37c
arexdu <710776009@qq.com>
2017-09-06 09:10:23 +0800

    add histogram mnist example

---
+++ tfl8.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+
+#number 1 to 10 data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None,):
+    # add one more layer and return the output of this layer
+    layer_name = 'layer%s' % n_layer
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size,out_size]),name='W')
+            tf.summary.histogram(layer_name+'/weights',Weights)
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1,out_size]) + 0.1,)
+            tf.summary.histogram(layer_name+'/biases',biases)
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b,)
+    tf.summary.histogram(layer_name+'/outputs',biases)
+    return outputs
+
+def compute_accuracy(v_xs,v_ys):
+    global prediction
+    y_pre = sess.run(prediction,feed_dict={xs:v_xs,ys:v_ys})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1),tf.argmax(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    result = sess.run(accuracy, feed_dict={xs:v_xs,ys:v_ys})
+    return result
+
+#define placeholder for inputs to neural network
+xs = tf.placeholder(tf.float32,[None,784]) #28x28
+ys = tf.placeholder(tf.float32,[None,10])
+
+#add output layer
+prediction = add_layer(xs,784,10,n_layer=1,activation_function=tf.nn.softmax)
+
+#the error between prediction and real data
+with tf.name_scope('cross_entropy'):
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),
+                                              reduction_indices=[1]))
+    tf.summary.scalar('cross_entropy',cross_entropy)
+
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+merged = tf.summary.merge_all()
+
+#important step
+writer = tf.summary.FileWriter('./',sess.graph)
+sess.run(tf.global_variables_initializer())
+
+for i in range(1000):
+    batch_xs,batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step,feed_dict={xs:batch_xs, ys:batch_ys})
+    if i%10 == 0:
+        print(compute_accuracy(
+            mnist.test.images,mnist.test.labels))
+        result = sess.run(merged, feed_dict={xs:batch_xs, ys:batch_ys})
+        writer.add_summary(result,i)
