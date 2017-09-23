22a209c9b4ea71318094891e6a5aa540e7e1f9c0
arexdu <710776009@qq.com>
2017-09-23 20:33:30 +0800

    sklearn update LinearRegression example

--- tfl16.py
+++ tfl16.py
@@ -12,14 +12,19 @@ worths=[ii*6.25 + np.random.normal(scale=40.) for ii in ages]
 
 ages=np.reshape(np.array(ages),(len(ages),1))
 worths=np.reshape(np.array(worths),(len(worths),1))
-plt.scatter(ages,worths)
-plt.xlabel("ages")
-plt.ylabel("worths")
-plt.show()
+
+
 from sklearn.linear_model import LinearRegression
 reg=LinearRegression()
 reg.fit(ages,worths)
-print("tom's worth prediction:", reg.prediction([27]))
+print("tom's worth prediction:", reg.predict([[27]]))
 print("r-squared score:",reg.score(ages,worths))
 print("slope:",reg.coef_)
 print("intercept:",reg.intercept_)
+
+plt.scatter(ages,worths)
+plt.plot(ages,reg.predict(ages),color='blue',linewidth=3)
+plt.xlabel("ages")
+plt.ylabel("worths")
+plt.show()
+
