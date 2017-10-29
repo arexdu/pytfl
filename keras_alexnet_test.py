d89e75534ddbbd4b52e4f5bf40b0c017f2746a02
arexdu <710776009@qq.com>
2017-10-29 14:33:22 +0800

    keras alexnet test

---
+++ keras_alexnet_test.py
@@ -0,0 +1,45 @@
+
+##Test AlexNet
+import numpy as np
+np.random.seed(42)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+
+
+import tflearn.datasets.oxflower17 as oxflower17
+X,Y = oxflower17.load_data(one_hot=True)
+
+
+##Model Design
+model = Sequential()
+model.add(Conv2D(96,kernel_size={11,11}, strides={4,4},activation='relu',input_shape={224,224,3}))
+model.add(MaxPooling2D(pool_size={3,3},strides={2,2}))
+model.add(BatchNormalization())
+
+model.add(Conv2D(256,kernel_size={5,5},activation='relu'))
+model.add(MaxPooling2D(pool_size={3,3},strides={2,2}))
+model.add(BatchNormalization())
+
+
+model.add(Conv2D(256,kernel_size={3,3},activation='relu'))
+model.add(Conv2D(384,kernel_size={3,3},activation='relu'))
+model.add(Conv2D(384,kernel_size={3,3},activation='relu'))
+model.add(MaxPooling2D(pool_size={3,3},strides={2,2}))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dense(4096,activation='tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(4096,activation='tanh'))
+model.add(Dropout(0.5))
+model.add(Dropout(17,activation='softmax'))
+          
+model.summary()
+
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+model.fit(X,Y,batch_size=64,epochs=100,verbose=1,validation_split=0.1,shuffle=True)
+
